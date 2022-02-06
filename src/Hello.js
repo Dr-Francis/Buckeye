da1c052485ec126d9fe62b30ad864c21dbe7d35f
Dr-Francis <audaxaly@gmail.com>
2022-02-06 19:44:29 +0100

    Setting up Buckeye project

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// Our Component Class
+class Hello extends React.Component {
+    render() {
+        return(<div>
+            <h1>Life is a big SMILE</h1>
+        </div>)
+    }
+}
+export default Hello
